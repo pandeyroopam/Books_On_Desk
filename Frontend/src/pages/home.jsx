90b53e6dbbdd4d6e95e2ba524a7e6b192bdf6eaa
pandeyroopam <roopampandey21@gmail.com>
2025-04-15 07:14:39 +0530

    Removed PascalCase files and kept lowercase home.jsx and home.css

--- Frontend/src/pages/home.jsx
+++ Frontend/src/pages/home.jsx
@@ -1,37 +1,30 @@
-import React from 'react'
-import './home.css';
-import Navbar from '../components/navbar'
-import  Carousel from '../components/carousel'
-import Mainbody from '../components/mainbody';
-import Footer from '../components/footer';
-import FormSection from '../components/formSection';
+import React from "react";
+import "./home.css";
+import Navbar from "../components/navbar";
+import Carousel from "../components/carousel";
+import Mainbody from "../components/mainbody";
+import Footer from "../components/footer";
+import FormSection from "../components/formSection";
 const home = () => {
   return (
-
     <>
-    <div className="nav-divs">
-      <Navbar/>
-    </div>
-    <div className="carousel-div">
-    <Carousel/>
-    </div>
-    <div className='landing-page-body'>
-      <Mainbody/>
-
-    </div>
-    <div className='form-input'>
-      <FormSection/>
-
-    </div>
-    <div className='Footer-div'>
-      <Footer/>
-
-    </div>
-    
-          
-
+      <div className="nav-divs">
+        <Navbar />
+      </div>
+      <div className="carousel-div">
+        <Carousel />
+      </div>
+      <div className="landing-page-body">
+        <Mainbody />
+      </div>
+      <div className="form-input">
+        <FormSection />
+      </div>
+      <div className="Footer-div">
+        <Footer />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default home
+export default home;
